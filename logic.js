b31d716bdb66ab0d2ac7e034d3a9f08216b8a80b
Rajib Paul <53445533+rajib3728@users.noreply.github.com>
2025-11-08 21:26:33 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,27 @@
+function openworkpage() 
+    {
+        window.location.href = "workpage.html";
+    }
+function fun1()
+{
+    window.location.href = "https://rajib3728.github.io/Meditation-Helper/";
+}
+function fun2()
+{
+    window.location.href = "https://rajib3728.github.io/motivation/";
+}
+function fun3()
+{
+    window.location.href = "https://rajib3728.github.io/sudoku/";
+}
+function fun4()
+{
+    window.location.href = "https://rajib3728.github.io/text-player/";
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
